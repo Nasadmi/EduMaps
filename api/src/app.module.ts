251734e2aaf516fb01a646d7e3931d0370c05f7f
nasadmi <samueldiego828@gmail.com>
2024-07-19 15:44:39 +0200

    Add MySQL connection

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST || 'localhost',
+        port: parseInt(process.env.DB_PORT) || 3306,
+        username: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || 'markmaps-app',
+        entities: [],
+        synchronize: process.env.NODE_ENV === 'dev',
+      }),
+    }),
+  ],
 })
 export class AppModule {}
