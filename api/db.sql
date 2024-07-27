d07a5655755e3d78de5ade023a8e8eca22e8a8aa
nasadmi <samueldiego828@gmail.com>
2024-07-27 16:53:09 +0200

    Add check_duplicate_markmaps on update trigger

--- api/db.sql
+++ api/db.sql
@@ -43,6 +43,21 @@ $$
 DELIMITER ;
 
 DELIMITER $$
+CREATE TRIGGER `check_duplicate_markmaps_on_update` BEFORE UPDATE ON `markmaps` FOR EACH ROW BEGIN
+	DECLARE markmaps_count INT;
+    SELECT COUNT(*)
+    INTO markmaps_count
+    FROM markmaps
+    WHERE name = NEW.name AND user = NEW.user;
+    IF markmaps_count > 0 THEN
+    	SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The user already has a markmap with this name.';
+       END IF;
+END
+$$
+DELIMITER ;
+
+DELIMITER $$
 CREATE TRIGGER `update_author_column` AFTER UPDATE ON 'users' FOR EACH ROW BEGIN
     IF NEW.username IS NOT NULL THEN
         UPDATE markmaps SET author = NEW.username WHERE user = NEW.id;
