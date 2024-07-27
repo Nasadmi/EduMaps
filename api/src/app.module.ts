6c6c15a78de1b407faed62c9fc4620cfedcb545d
nasadmi <samueldiego828@gmail.com>
2024-07-27 16:52:26 +0200

    Add markmaps entity/module and user subscriber

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,25 +3,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { User } from './entities/user.entity';
+import { Markmaps } from './entities/markmaps.entity';
+import { MarkmapsModule } from './markmaps/markmaps.module';
+import { UserSubscriber } from './subscribers/user.subscriber';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      useFactory: async () => ({
-        type: 'mysql',
-        host: process.env.DB_HOST || 'localhost',
-        port: parseInt(process.env.DB_PORT) || 3306,
-        username: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '',
-        database: process.env.DB_NAME || 'markmaps-app',
-        entities: [User],
-        synchronize: process.env.NODE_ENV === 'dev',
-      }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME || 'markmaps-app',
+      entities: [User, Markmaps],
+      subscribers: [UserSubscriber],
+      synchronize: process.env.NODE_ENV === 'dev',
     }),
     UserModule,
+    MarkmapsModule,
   ],
 })
 export class AppModule {}
