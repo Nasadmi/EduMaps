a8f4ed47054b42e43e3bbababfba385a0f2198c1
nasadmi <samueldiego828@gmail.com>
2024-08-15 23:10:39 +0200

    Enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,16 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: (origin, callback) => {
+      const whitelist = ['http://localhost:3000', 'http://localhost:4200'];
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS policy'));
+      }
+    },
+  });
   await app.listen(3000);
 }
 bootstrap();
