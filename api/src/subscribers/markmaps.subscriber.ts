1b5bd31b98e97527b041f9678472845bd3f15b59
nasadmi <samueldiego828@gmail.com>
2024-08-21 16:11:18 +0200

    Add auto author set in beforeInsert

--- api/src/subscribers/markmaps.subscriber.ts
+++ api/src/subscribers/markmaps.subscriber.ts
@@ -29,6 +29,20 @@ export class MarkmapsSubscriber implements EntitySubscriberInterface<Markmaps> {
         'The user already has a markmap with this name.',
       );
     }
+
+    try {
+      const author = (await event.queryRunner.query(
+        'SELECT username FROM user WHERE id = ?',
+        [event.entity.user],
+      )) as { username: string }[] | null;
+      if (!author) {
+        throw new BadRequestException('The user id is invalid.');
+      } else {
+        event.entity.author = author[0].username;
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   async beforeUpdate(event: UpdateEvent<Markmaps>) {
