f05517772e4705ecf9b090942248d1f061891c62
nasadmi <samueldiego828@gmail.com>
2024-08-24 01:37:34 +0200

    Configure ejs and public files to main.ts and update cors whitelist

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { type NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: (origin, callback) => {
-      const whitelist = ['http://localhost:3000', 'http://localhost:4200'];
+      const whitelist = [process.env.DOMAIN];
       if (!origin || whitelist.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
@@ -15,6 +17,13 @@ async function bootstrap() {
       }
     },
   });
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    index: false,
+  });
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+  app.setLocal('title', 'EduMaps');
+  app.setLocal('domain', process.env.DOMAIN);
   await app.listen(3000);
 }
 bootstrap();
