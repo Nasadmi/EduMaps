b564e16d38f2add32e6a38f78f7209a783baecdf
Nasadmi <samueldiego828@gmail.com>
2024-10-15 00:17:23 +0200

    Add: Implements TypeORM

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmConfigService } from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
